d4d32559299922476740422e8c5281a676c396cc
ramarao1nani <nuthulapatiramarao789@gmail.com>
2022-09-25 14:51:07 +0530

    allowed hosts

--- news/settings.py
+++ news/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = ['https://boiling-harbor-88924.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
